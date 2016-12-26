0a9d6b83393c6436de5fb8460557c3015f8c3721
gong <15933688686@163.com>
2016-12-26 17:11:53 +0800

    hdy 1711

--- app/src/main/java/com/jims/work/MainActivity.java
+++ app/src/main/java/com/jims/work/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //gong
+        //hdy
         setContentView(R.layout.activity_main);
     }
 }
