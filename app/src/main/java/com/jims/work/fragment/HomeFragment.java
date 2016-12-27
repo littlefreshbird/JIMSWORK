73f2f055abd1b03875ff8dca8f3a0d1545ea89c1
gong <15933688686@163.com>
2016-12-27 15:36:58 +0800

    gong

---
+++ app/src/main/java/com/jims/work/fragment/HomeFragment.java
@@ -0,0 +1,11 @@
+package com.jims.work.fragment;
+
+
+import com.jims.work.fragment.base.BaseFragment;
+
+/**
+ * Created by gong on 2016/12/27.
+ */
+
+public class HomeFragment extends BaseFragment {
+}
---
+++ app/src/main/java/com/jims/work/fragment/base/BaseFragment.java
@@ -0,0 +1,17 @@
+package com.jims.work.fragment.base;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gong on 2016/12/27.
+ */
+public class BaseFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
