a96bfc3527b88e8cde340287d6c674da99c4efa8
106104695@qq.com <106104695@qq.com>
2016-12-30 11:00:31 +0800

    1

--- app/src/main/java/com/jims/work/RegisterActivity.java
+++ app/src/main/java/com/jims/work/RegisterActivity.java
@@ -1,16 +1,24 @@
 package com.jims.work;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.img_back)
+    ImageView imgBack;
+    @BindView(R.id.layout_login_topbar)
+    RelativeLayout layoutLoginTopbar;
     @BindView(R.id.edit_mobile)
     EditText editMobile;
     @BindView(R.id.access_password)
@@ -28,7 +36,7 @@ public class RegisterActivity extends AppCompatActivity {
         accessNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(RegisterActivity.this,RegisterNextActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, RegisterNextActivity.class);
                 startActivity(intent);
             }
         });
