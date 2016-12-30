1423c3cd26591b901b608c9b4140daa4d9b0af0b
106104695@qq.com <106104695@qq.com>
2016-12-30 10:38:28 +0800

    1

--- app/src/main/java/com/jims/work/LoginActivity.java
+++ app/src/main/java/com/jims/work/LoginActivity.java
@@ -4,34 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 
 public class LoginActivity extends BaseActivity {
 
-
-
-    @BindView(R.id.etAccount)
-    EditText etAccount;
-    @BindView(R.id.etPassWord)
-    EditText etPassWord;
-    @BindView(R.id.login)
-    Button btnlogin;
-    @BindView(R.id.zhaohuipass)
-    TextView zhaohuipass;
-    @BindView(R.id.btn_register)
-    TextView btnRegister;
+private Button btnlogin;
+    private TextView btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ButterKnife.bind(this);
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        btnlogin=(Button)findViewById(R.id.login);
+        btnRegister= (TextView) findViewById(R.id.btn_register);
+       btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
