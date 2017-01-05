d31ba22718ce06e0ebd489a94e5735f68f63e60a
106104695@qq.com <106104695@qq.com>
2017-01-05 09:25:58 +0800

    hdy

--- app/src/main/java/com/jims/work/RegisterNextActivity.java
+++ app/src/main/java/com/jims/work/RegisterNextActivity.java
@@ -1,13 +1,45 @@
 package com.jims.work;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegisterNextActivity extends AppCompatActivity {
+
+    @BindView(R.id.img_registernext_back)
+    ImageView imgRegisternextBack;
+    @BindView(R.id.edit_code)
+    EditText editCode;
+    @BindView(R.id.edit_copycode)
+    EditText editCopycode;
+    @BindView(R.id.btn_register_next)
+    Button btnRegisterNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_next);
+        ButterKnife.bind(this);
+        imgRegisternextBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterNextActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnRegisterNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterNextActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
