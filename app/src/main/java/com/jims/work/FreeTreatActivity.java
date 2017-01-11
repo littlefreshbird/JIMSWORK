6419325ab95b12e4e9ede1a3fa2ce7f840d324c8
gong <15933688686@163.com>
2017-01-11 18:58:37 +0800

    gong

--- app/src/main/java/com/jims/work/FreeTreatActivity.java
+++ app/src/main/java/com/jims/work/FreeTreatActivity.java
@@ -1,12 +1,42 @@
 package com.jims.work;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +59,18 @@ public class FreeTreatActivity extends AppCompatActivity {
     @BindView(R.id.button_1)
     Button button1;
 
+
+    private String pathImage;
+    private Context mContext;
+    private ArrayList<HashMap<String, Object>> imageItem;
+    private SimpleAdapter simpleAdapter;     //适配器private static final int PHOTO_SUCCESS = 1;
+    private Bitmap bmp;
+    private static final int PHOTO_SUCCESS = 1;
+    private static final int CAMERA_SUCCESS = 2;
+    private boolean bl = false;
+    private int position1 = 0;
+    private final int IMAGE_OPEN = 1;               //打开图片标记
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,11 +81,247 @@ public class FreeTreatActivity extends AppCompatActivity {
     }
 
     private void initview() {
+        mContext = FreeTreatActivity.this;
         image.setImageResource(R.drawable.doctor1);
         patientname.setText("姓名：李国胜");
         patientage.setText("年龄：28");
         patientsex.setText("性别：男");
+        setCustomActionBar();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.
+                SOFT_INPUT_ADJUST_PAN);
+        //锁定屏幕
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.gridview_addpic); //加号
+        imageItem = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("itemImage", bmp);
+        imageItem.add(map);
+        simpleAdapter = new SimpleAdapter(this,
+                imageItem, R.layout.griditem_addpic,
+                new String[]{"itemImage"}, new int[]{R.id.imageView1});
+        /*
+         *              1.自定义继承BaseAdapter实现
+         *              2.ViewBinder()接口实现
+         */
+        simpleAdapter.setViewBinder(new SimpleAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Object data,
+                                        String textRepresentation) {
+                if (view instanceof ImageView && data instanceof Bitmap) {
+                    ImageView i = (ImageView) view;
+                    i.setImageBitmap((Bitmap) data);
+                    return true;
+                }
+                return false;
+            }
+        });
+        gridView.setAdapter(simpleAdapter);
 
+        /*
+         * 监听GridView点击事件
+         * 报错:该函数必须抽象方法 故需要手动导入import android.view.View;
+         */
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                if (imageItem.size() == 6) { //第一张为默认图片
+                    Toast.makeText(mContext, "图片数5张已满", Toast.LENGTH_SHORT).show();
+                } else if (position == 0) { //点击图片位置为+ 0对应0张图片
+                    final CharSequence[] items = {"手机相册", "相机拍摄"};
+                    AlertDialog dlg = new AlertDialog.Builder(mContext).setTitle("选择图片").setItems(items,
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int item) {
+                                    if (item == 1) {
+                                        Intent getImageByCamera = new Intent("android.media.action.IMAGE_CAPTURE");
+                                        startActivityForResult(getImageByCamera, CAMERA_SUCCESS);
+                                    } else {
+                                        Intent intent = new Intent(Intent.ACTION_PICK,
+                                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                        startActivityForResult(intent, IMAGE_OPEN);
+                                    }
+                                }
+                            }).create();
+                    dlg.show();
+                    /*Toast.makeText(PostActivity.this, "添加图片", Toast.LENGTH_SHORT).show();
+                    //选择图片
+                    Intent intent = new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, IMAGE_OPEN);*/
+                    //通过onResume()刷新数据
+                } else {
+                    //  dialog(position); //增加点击放大效果
+                    position1 = position;
+                    if (bl == false) {
+                        Animation testAnim = AnimationUtils.loadAnimation(mContext, R.anim.anim);
+                        gridView.getChildAt(position).startAnimation(testAnim);
+                        bl = true;
+                    } else {
+                        bl = false;
+                        Animation testAnim = AnimationUtils.loadAnimation(mContext, R.anim.anim1);
+                        gridView.getChildAt(position).startAnimation(testAnim);
+                    }
+                }
+            }
+        });
+    }
+
+    //调用menu中的main资源
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mf = getMenuInflater();
+        mf.inflate(R.menu.freetreatmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //点击事件
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent1 = new Intent(mContext, MainActivity.class);//点击回主页
+                startActivity(intent1);
+                finish();
+            case R.id.action:
+                openInputMethod(edit);
+                return true;
+            case R.id.deletephoto:
+                if (position1 == 0) {
+                    Toast.makeText(this, "不能删除", Toast.LENGTH_SHORT).show();
+                    return false;
+                } else {
+                    dialog(position1);//删除照片
+                    return true;
+                }
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * @Description:关闭系统软键盘
+     */
+    public void closeInputMethod() {
+        try {
+            ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                    .hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (Exception e) {
+        } finally {
+        }
+    }
+
+    /**
+     * @Description:打开系统软键盘
+     */
+    public void openInputMethod(final EditText editText) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                InputMethodManager inputManager = (InputMethodManager) editText
+                        .getContext().getSystemService(
+                                Context.INPUT_METHOD_SERVICE);
+                inputManager.showSoftInput(editText, 0);
+
+            }
+
+        }, 200);
+    }
+
+    //获取图片路径 响应startActivityForResult
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //打开图片
+        if (resultCode == RESULT_OK && requestCode == IMAGE_OPEN) {
+            Uri uri = data.getData();
+            if (!TextUtils.isEmpty(uri.getAuthority())) {
+                //查询选择图片
+                Cursor cursor = getContentResolver().query(
+                        uri,
+                        new String[]{MediaStore.Images.Media.DATA},
+                        null,
+                        null,
+                        null);
+                //返回 没找到选择图片
+                if (null == cursor) {
+                    return;
+                }
+                //光标移动至开头 获取图片路径
+                cursor.moveToFirst();
+                pathImage = cursor.getString(cursor
+                        .getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+        }  //end if 打开图片
+    }
+
+    //刷新图片
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!TextUtils.isEmpty(pathImage)) {
+            Bitmap addbmp = BitmapFactory.decodeFile(pathImage);
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("itemImage", addbmp);
+            imageItem.add(map);
+            simpleAdapter = new SimpleAdapter(this,
+                    imageItem, R.layout.griditem_addpic,
+                    new String[]{"itemImage"}, new int[]{R.id.imageView1});
+            simpleAdapter.setViewBinder(new SimpleAdapter.ViewBinder() {
+                @Override
+                public boolean setViewValue(View view, Object data,
+                                            String textRepresentation) {
+                    // TODO Auto-generated method stub
+                    if (view instanceof ImageView && data instanceof Bitmap) {
+                        ImageView i = (ImageView) view;
+                        i.setImageBitmap((Bitmap) data);
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            gridView.setAdapter(simpleAdapter);
+            simpleAdapter.notifyDataSetChanged();
+            //刷新后释放防止手机休眠后自动添加
+            pathImage = null;
+        }
+    }
+
+    protected void dialog(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("确认移除已添加图片吗？");
+        builder.setTitle("提示");
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                imageItem.remove(position);
+                simpleAdapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    public void setCustomActionBar() {
+        ActionBar.LayoutParams lp = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT, Gravity.CENTER);
+        View mActionBarView = LayoutInflater.from(this).inflate(R.layout.actionbar, null);
+        TextView textView=ButterKnife.findById(mActionBarView,android.R.id.title);
+        textView.setText("提交义诊");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setCustomView(mActionBarView, lp);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.action_bar_back);
+        actionBar.setElevation(2);
     }
 
     @OnClick(R.id.button_1)
