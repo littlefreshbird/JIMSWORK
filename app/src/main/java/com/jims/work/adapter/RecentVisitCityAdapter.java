6d23d9c6b04275d83d4dbab3ec667ef6ea257e26
106104695@qq.com <106104695@qq.com>
2017-01-12 11:41:20 +0800

    hdy

--- app/src/main/java/com/jims/work/adapter/RecentVisitCityAdapter.java
+++ app/src/main/java/com/jims/work/adapter/RecentVisitCityAdapter.java
@@ -1,14 +1,15 @@
 package com.jims.work.adapter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.jims.work.CityPositionActivity;
 import com.jims.work.R;
 
 import java.util.List;
@@ -58,7 +59,19 @@ public class RecentVisitCityAdapter extends BaseAdapter {
 
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(mContext,mRecentVisitCityList.get(position)+"",0).show();
+				//Toast.makeText(mContext,mRecentVisitCityList.get(position)+"",0).show();
+
+
+				SharedPreferences pref = mContext.getSharedPreferences("data",Context.MODE_PRIVATE);
+				SharedPreferences.Editor editor = pref.edit();
+				editor.putString("CITY",mRecentVisitCityList.get(position)+"");
+				editor.commit();
+				if(CityPositionActivity.class.isInstance(mContext))
+				{
+					// 转化为activity，然后finish就行了
+					CityPositionActivity activity = (CityPositionActivity)mContext;
+					activity.finish();
+				}
 			}
 		});
 		return convertView;
