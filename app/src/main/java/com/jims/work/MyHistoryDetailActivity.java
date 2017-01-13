50332c6d1642a8402bc29fb17c1a19f600550dc0
hdy <106104695@qq.com>
2017-01-13 16:41:03 +0800

    hdy

---
+++ app/src/main/java/com/jims/work/MyHistoryDetailActivity.java
@@ -0,0 +1,13 @@
+package com.jims.work;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyHistoryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myhistory_detail);
+    }
+}
