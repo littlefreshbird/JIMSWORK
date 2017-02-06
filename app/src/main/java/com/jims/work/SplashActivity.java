7d0fb5c28367aac344311fcf383e02010a2d896a
hdy <106104695@qq.com>
2017-02-06 11:50:39 +0800

    hdy

--- app/src/main/java/com/jims/work/SplashActivity.java
+++ app/src/main/java/com/jims/work/SplashActivity.java
@@ -12,13 +12,17 @@ import android.view.Window;
  */
 public class SplashActivity extends AppCompatActivity {
     Handler mHandler=new Handler();
-
+   // private MyCountDownTimer mc;
+   // private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SplashActivity.this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
         getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
+       // tv = (TextView) findViewById(R.id.textView);
+       // mc = new MyCountDownTimer(3000, 1000);
+       // mc.start();
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -41,4 +45,34 @@ public class SplashActivity extends AppCompatActivity {
             }
         },3000);
     }
+    /*private Handler handler=new Handler();*//**
+     * 继承 CountDownTimer 防范
+     *
+     * 重写 父类的方法 onTick() 、 onFinish()
+     *//*class MyCountDownTimer extends CountDownTimer {
+        *//**
+         *
+         * @param millisInFuture
+         *      表示以毫秒为单位 倒计时的总数
+         *
+         *      例如 millisInFuture=1000 表示1秒
+         *
+         * @param countDownInterval
+         *      表示 间隔 多少微秒 调用一次 onTick 方法
+         *
+         *      例如: countDownInterval =1000 ; 表示每1000毫秒调用一次onTick()
+         *
+         *//*public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        public void onFinish() {
+            tv.setText("正在跳转");
+        }
+
+        public void onTick(long millisUntilFinished) {
+            tv.setText("倒计时(" + millisUntilFinished / 1000 + ")");
+        }
+
+    }*/
 }
