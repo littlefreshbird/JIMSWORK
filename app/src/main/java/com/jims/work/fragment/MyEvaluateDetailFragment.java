c0105e6ef8c06d900d82cedd4a9026bb6d0a4063
hdy <106104695@qq.com>
2017-02-10 16:10:16 +0800

    hdy

--- app/src/main/java/com/jims/work/fragment/MyEvaluateDetailFragment.java
+++ app/src/main/java/com/jims/work/fragment/MyEvaluateDetailFragment.java
@@ -1,5 +1,10 @@
 package com.jims.work.fragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,7 +13,10 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.jims.work.MoreActivity;
 import com.jims.work.R;
 
 import java.util.ArrayList;
@@ -48,7 +56,7 @@ public class MyEvaluateDetailFragment extends Fragment {
             listItems.add(listItem);
         }
 
-        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), listItems,
+        MySimpleAdapter simpleAdapter = new MySimpleAdapter(getActivity(), listItems,
                 R.layout.evaluate_detail_item,
                 new String[] { "imageids", "name", "desc","time","client","count","sexmaletop"   },
                 new int[] { R.id.tweet_listitem_userface, R.id.tweet_listitem_username,R.id.tweetcontent ,R.id.questiontime,R.id.tweet_listitem_client,R.id.tweet_listitem_commentCount,R.id.sexmaletop});
@@ -61,5 +69,85 @@ public class MyEvaluateDetailFragment extends Fragment {
 
 
     }
+    private class MySimpleAdapter extends SimpleAdapter {
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+            View v = super.getView(position, convertView, parent);
 
+            TextView add_myevaluate=(TextView) v.findViewById(R.id.add_myevaluate);
+            add_myevaluate.setTag(position);
+            add_myevaluate.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    Intent intent = new Intent(getActivity(),
+                            MoreActivity.class);
+                    startActivity(intent);
+
+
+                }
+            });
+            TextView del_myevaluate=(TextView) v.findViewById(R.id.del_myevaluate);
+            del_myevaluate.setTag(position);
+            del_myevaluate.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    dialog();
+
+
+                }
+            });
+            TextView update_myevaluate=(TextView) v.findViewById(R.id.update_myevaluate);
+            update_myevaluate.setTag(position);
+            update_myevaluate.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    Intent intent = new Intent(getActivity(),
+                            MoreActivity.class);
+                    startActivity(intent);
+
+
+                }
+            });
+            return v;
+        }
+
+        public MySimpleAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+            super(context, data, resource, from, to);
+            // TODO Auto-generated constructor stub
+        }
+    }
+    private void dialog(){
+        //先new出一个监听器，设置好监听
+        DialogInterface.OnClickListener dialogOnclicListener=new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch(which){
+                    case Dialog.BUTTON_POSITIVE:
+                        Toast.makeText(getActivity(), "确认" + which, Toast.LENGTH_SHORT).show();
+                        break;
+                    case Dialog.BUTTON_NEGATIVE:
+                        Toast.makeText(getActivity(), "取消" + which, Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+        };
+        //dialog参数设置
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());  //先得到构造器
+        builder.setTitle("提示"); //设置标题
+        builder.setMessage("确定删除这条评论吗?"); //设置内容
+        builder.setIcon(R.mipmap.ic_launcher);//设置图标，图片id即可
+        builder.setPositiveButton("确认",dialogOnclicListener);
+        builder.setNegativeButton("取消", dialogOnclicListener);
+
+        builder.create().show();
+    }
 }
